3625645c7029b4d36ff146123aacdd6275e344c2
grzegorz wojcikiewicz <wojcikiewiczgrzegorz@gmail.com>
2014-04-30 14:52:09 +0200

    Zrobiona gra dla dwoch graczy na jednym telefonie

--- OX/src/grigmisiek/ox/PlayActivity.java
+++ OX/src/grigmisiek/ox/PlayActivity.java
@@ -2,27 +2,41 @@ package grigmisiek.ox;
 
 import android.os.Bundle;
 import android.view.View.OnClickListener;
+import static android.view.View.*;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.app.Activity;
 //import android.view.Menu;
 
+enum User {X, O}
+
 public class PlayActivity extends Activity {
 	
 	TextView resultText;
-	ImageButton[] iButtonsTable;
+	TextView activeX;
+	TextView activeO;
+	ImageButton[] ibt;
 	int licznik;
+	User user;
+	boolean koniec;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_play);
+		
+		user = User.X;
+		licznik = 0;
+		koniec = false;
+		activeX = (TextView) findViewById(R.id.activeX);
+		activeO = (TextView) findViewById(R.id.activeO);
+		activeX.setVisibility(VISIBLE);
+		activeO.setVisibility(INVISIBLE);
+		
 		resultText = (TextView) findViewById(R.id.resultText);
 		
-		licznik = 0;
-		
-		iButtonsTable = new ImageButton[] {
+		ibt = new ImageButton[] {
 				(ImageButton) findViewById(R.id.imageButton1),
 				(ImageButton) findViewById(R.id.imageButton2),
 				(ImageButton) findViewById(R.id.imageButton3),
@@ -34,18 +48,60 @@ public class PlayActivity extends Activity {
 				(ImageButton) findViewById(R.id.imageButton9)
 		};
 		
-		for (int i=0; i<iButtonsTable.length; i++) {
-			iButtonsTable[i].setOnClickListener(new OnClickListener() {
+		for (int i=0; i<ibt.length; i++) {
+			ibt[i].setOnClickListener(new OnClickListener() {
 				@Override
 				public void onClick(View v) {
 					ImageButton ib = (ImageButton) findViewById(v.getId());
-					ib.setImageResource(R.drawable.x);
+					
+					if(koniec || ib.getDrawable() != null){
+						return;
+					}
+					
+					if(user == User.X){
+						ib.setImageResource(R.drawable.x);
+						ib.setTag(User.X);
+						if(isWinner(user)){
+							resultText.setText(R.string.winX);
+							koniec = true;
+							return;
+						}
+						user = User.O;
+						activeX.setVisibility(INVISIBLE);
+						activeO.setVisibility(VISIBLE);
+					} else {
+						ib.setImageResource(R.drawable.o);
+						ib.setTag(User.O);
+						if(isWinner(user)){
+							resultText.setText(R.string.winO);
+							koniec = true;
+							return;
+						}
+						user = User.X;
+						activeX.setVisibility(VISIBLE);
+						activeO.setVisibility(INVISIBLE);
+					}
 					
 					if(++licznik == 9)
 						resultText.setText(R.string.draw);
+					
 				}
 			});
 		}
+	}
+	
+	private boolean isWinner(User user) {
+		if (ibt[0].getTag() == user && ibt[1].getTag() == user && ibt[2].getTag() == user ||
+				ibt[3].getTag() == user && ibt[4].getTag() == user && ibt[5].getTag() == user ||
+				ibt[6].getTag() == user && ibt[7].getTag() == user && ibt[8].getTag() == user ||
+				ibt[0].getTag() == user && ibt[3].getTag() == user && ibt[6].getTag() == user ||
+				ibt[1].getTag() == user && ibt[4].getTag() == user && ibt[7].getTag() == user ||
+				ibt[2].getTag() == user && ibt[5].getTag() == user && ibt[8].getTag() == user ||
+				ibt[0].getTag() == user && ibt[4].getTag() == user && ibt[8].getTag() == user ||
+				ibt[2].getTag() == user && ibt[4].getTag() == user && ibt[6].getTag() == user) {
+			return true;
+		}
+		return false;
 	}
 
 	/*@Override
